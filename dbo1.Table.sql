a8eed405e105436ccf0d664c6fca9eb7c3334bb9
Amitgoswami12 <98880561+Amitgoswami12@users.noreply.github.com>
2023-07-17 09:57:58 +0530

    Add files via upload

---
+++ dbo1.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table](
+ [Id] VARCHAR(MAX) NOT NULL PRIMARY KEY,     [Enrollment no.] NCHAR(10) NULL, 
+    [Name] NCHAR(10) NULL,     [Branch] NCHAR(10) NULL, 
+    [Sem] INT NULL,     [Gender] NCHAR(10) NULL, 
+    [Mobile No.] NCHAR(10) NULL,     [DOB] NCHAR(10) NULL
+ 
+)
---
+++ dbo2.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table](
+ [Id] VARCHAR(MAX) NOT NULL PRIMARY KEY,     [Enrollmentno] NCHAR(10) NULL, 
+    [Name] NCHAR(10) NULL,     [Address] NCHAR(10) NULL, 
+    [Branch] INT NULL,     [Semester] NCHAR(10) NULL, 
+    [Gender] NCHAR(10) NULL,     [MobileNo] NCHAR(10) NULL, 
+    [DOB] NCHAR(10) NULL
+ 
+)
